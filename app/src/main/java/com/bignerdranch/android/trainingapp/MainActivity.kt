12c1fc317c7475f1cdbd9f0c1e54d881ed87c64c
Tujh555 <104990001+Tujh555@users.noreply.github.com>
2022-05-26 23:17:35 +0300

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/trainingapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.bignerdranch.android.trainingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.tv)
+        textView.text = "HELLO WORLD"
+    }
+}
